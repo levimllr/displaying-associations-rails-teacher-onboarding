d39ba3eb14f7a5c9e249b3cfe658b7e7fa5651f0
Levi Miller <paullevimiller@gmail.com>
2019-08-16 10:30:00 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,12 @@ class PostsController < ApplicationController
 
 	def update
 		post = Post.find(params[:id])
-		post.update(params.require(:post))
+		post.update(post_params)
 		redirect_to post_path(post)
+	end
+
+	private
+	def post_params
+	  params.require(:post).permit(:title, :description, :category_id)
 	end
 end
